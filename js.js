4c2823554e474ee21020f98c555f39cf72f0bfa8
Makis Spyratos <makis>
2017-09-18 10:47:44 +0100

    ok
    ok

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
 
 var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 500}});
 
-let arr = document.querySelectorAll("div[type=round]");
+let arr = document.querySelectorAll("div.round");
 
 
 
@@ -21,11 +21,53 @@ let arr = document.querySelectorAll("div[type=round]");
 })
 
 
-var LB = document.querySelector("#LB")
-var IMG = document.querySelector("#IMG-VIEW")
-
-function openLighBox(path){
 
 
+var collection02 = {
+  A002001:{
+    price:"500",
+    sold : true,
+    tag : "|&nbsp;&nbsp;||&nbsp;&nbsp;&nbsp;&nbsp;|",
+    description:'A3 16.5" x 11.7". marker on paper',
+    art : "img/collection02/art-space.jpg"
+  },
+  A002002:{
+    price:"800",
+    sold : false,
+    tag : "|&nbsp;&nbsp;||&nbsp;&nbsp;||",
+    description:'A3 16.5" x 11.7". marker on paper',
+    art : "img/collection02/art-pope.jpg"
+  }
 
 }
+
+
+var source   = document.getElementById("art-template").innerHTML
+var template = Handlebars.compile(source)
+var detailEl = document.getElementById("art-window")
+
+function openLighBox(obj){
+
+
+    detailEl.className="open"
+    detailEl.innerHTML=template(obj)
+
+    setTimeout(function(){
+        $("#art-img").pinchzoomer()
+    },0)
+
+}
+function closeLightBox(){
+    detailEl.className=""
+}
+
+detailEl.addEventListener("click",function(evt){
+
+      closeLightBox()
+})
+
+document.querySelector("#collection002").addEventListener("click",function(evt){
+
+      if(evt.srcElement.className==="round")
+      openLighBox(collection02[evt.srcElement.id])
+})
