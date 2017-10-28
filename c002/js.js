b667aea62db43c57600722140b9ca00550f89e1e
Makis Spyratos <makis>
2017-10-28 14:07:03 +0100

    dewed
    wdwedw

--- c002/js.js
+++ c002/js.js
@@ -18,14 +18,21 @@ var collection02 = {
     sold : false,
     tag : "|&nbsp;&nbsp;||&nbsp;&nbsp;||",
     description:'A3 16.5" x 11.7". marker on paper',
-    art : "c002/art-bum.jpg"
+    art : "c002/art-dots.jpg"
   },
   A002004:{
-    price:"800",
+    price:"1200",
     sold : false,
     tag : "|&nbsp;&nbsp;||&nbsp;&nbsp;||",
     description:'A3 16.5" x 11.7". marker on paper',
     art : "c002/art-bum.jpg"
+  },
+  A002005:{
+    price:"200",
+    sold : false,
+    tag : "|&nbsp;&nbsp;||&nbsp;&nbsp;||",
+    description:'A3 16.5" x 11.7". marker on paper',
+    art : "c002/art-trap.jpg"
   }
 
 }
@@ -35,34 +42,39 @@ var source   = document.getElementById("art-template").innerHTML
 var template = Handlebars.compile(source)
 
 
-
 function openLighBox(obj){
 
     obj = collection02[obj.id]
 
-    $('#art-window').addClass("open")
+    ARTWIN.addClass("open")
     $('#art-details').html(template(obj))
     $(document.body).css('overflow','hidden')
 
 
-    setTimeout(function(){
-          viewer.load(obj.art)
-          $('#art-window').addClass("ready")
-    },600)
+    viewer.load(obj.art)
+    ARTWIN.addClass("ready")
+
 
 }
-function closeLightBox(){
-    $('#art-window').removeClass("ready").removeClass("open")
 
-    $(document.body).css('overflow','hidden')
+function closeLightBox(){
+    ARTWIN.addClass("close").removeClass("ready").removeClass('open')
+    setTimeout(function(){
+
+        ARTWIN.removeClass('close')
+    },700)
+    $(document.body).css('overflow','auto')
 }
 
 var viewer
+var ARTWIN
 
 $(document).ready(function(){
 
+    ARTWIN = $('#art-window')
+
     viewer = ImageViewer('#art-image',{
-    
+      zoomValue : 200
     })
 
 
@@ -89,6 +101,12 @@ new ScrollMagic.Scene({triggerElement: "#A002005", triggerHook: "onLeave"})
 							.setClassToggle("#nav", "open")
 							.addTo(controller)
 
-new ScrollMagic.Scene({triggerElement: "#A002005", duration:200, triggerHook: "1"})
+new ScrollMagic.Scene({triggerElement: "#A002004", duration:200, triggerHook: "0.4"})
 							.setClassToggle("#intro-viz", "out")
 							.addTo(controller)
+
+
+  new ScrollMagic.Scene({triggerElement: "#illustration-splash", triggerHook: "0.02"})
+  							.setClassToggle("#illustration-splash-img", "fade-in")
+                .reverse(false)
+  							.addTo(controller)
